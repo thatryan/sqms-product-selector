067f93ac0304d3530293dd9950a00c1a6cfe9214
Ryan Olson <ryan@thatryan.com>
2016-09-23 18:23:30 -0700

    attempt form advance with gform anchor

--- assets/js/sqms-product-selector.js
+++ assets/js/sqms-product-selector.js
@@ -2,7 +2,7 @@ var gfp_auto_advance_page_js = {"fields":["3","21","25","22","4","37","6","65","
 
 function gfp_add_auto_advance_field_events(_, e) {
     if (e == gfp_auto_advance_page_js.form_id) {
-        jQuery(".gform_previous_button").hide(), jQuery(".gform_next_button").hide(), jQuery("#gform_submit_button_" + gfp_auto_advance_page_js.form_id).hide();
+        // jQuery(".gform_previous_button").hide(), jQuery(".gform_next_button").hide(), jQuery("#gform_submit_button_" + gfp_auto_advance_page_js.form_id).hide();
         for (var a = 0; a < gfp_auto_advance_page_js.fields.length; a++) jQuery("input[name='input_" + gfp_auto_advance_page_js.fields[a] + "'],select[name='input_" + gfp_auto_advance_page_js.fields[a] + "']").change(gfp_auto_advance_page);
     }
 }
