cdf4a7f33c62312a37ee54b96d8a9526758dfa31
Ryan Olson <ryan@thatryan.com>
2016-10-18 17:59:27 -0700

    add logging check to debug auto advance

--- assets/js/sqms-product-selector.js
+++ assets/js/sqms-product-selector.js
@@ -1,14 +1,19 @@
 var gfp_auto_advance_page_js = {"fields":["3","21","25","22","4","37","6","65","27","59"],"form_id":"12"};
 
 function gfp_add_auto_advance_field_events(_, e) {
+    console.log( e );
     if (e == gfp_auto_advance_page_js.form_id) {
-        // jQuery(".gform_previous_button").hide(), jQuery(".gform_next_button").hide(), jQuery("#gform_submit_button_" + gfp_auto_advance_page_js.form_id).hide();
-        for (var a = 0; a < gfp_auto_advance_page_js.fields.length; a++) jQuery("input[name='input_" + gfp_auto_advance_page_js.fields[a] + "'],select[name='input_" + gfp_auto_advance_page_js.fields[a] + "']").change(gfp_auto_advance_page);
+
+        for (var a = 0; a < gfp_auto_advance_page_js.fields.length; a++){
+            jQuery("input[name='input_" + gfp_auto_advance_page_js.fields[a] + "'],select[name='input_" + gfp_auto_advance_page_js.fields[a] + "']").change(gfp_auto_advance_page);
+        }
     }
 }
 function gfp_auto_advance_page() {
     var _ = jQuery(this).parents(".gform_page").find(".gform_next_button");
-    if (0 < _.length) _.trigger("click");
+    if (0 < _.length){
+        _.trigger("click");
+    }
     else {
         var e = jQuery(this).parents(".gform_page").find("#gform_submit_button_" + gfp_auto_advance_page_js.form_id);
         0 < e.length && e.trigger("click");
