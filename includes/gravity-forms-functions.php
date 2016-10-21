4a20afc0eea525a1b12afe41dc922c557d2fef27
Ryan Olson <ryan@thatryan.com>
2016-10-20 18:21:26 -0700

    remove dealer display

--- includes/gravity-forms-functions.php
+++ includes/gravity-forms-functions.php
@@ -9,15 +9,15 @@ add_filter( 'gform_pre_render_12', 'create_dynamic_seer_dropdown' );
 add_filter( 'gform_pre_render_12', 'create_dynamic_eff_dropdown' );
 add_filter( 'gform_pre_render_12', 'create_dynamic_orientation_dropdown' );
 
-add_filter( 'gform_pre_render_12', 'build_dealer_list' );
+// add_filter( 'gform_pre_render_12', 'build_dealer_list' );
 add_filter( 'gform_pre_render_16', 'build_dealer_list' );
 
 add_filter( 'gform_pre_render_12', 'display_choice_result' );
 
-add_filter( 'gform_notification_12', 'get_dealer_email', 10, 3 );
+// add_filter( 'gform_notification_12', 'get_dealer_email', 10, 3 );
 add_filter( 'gform_notification_16', 'get_dealer_email', 10, 3 );
 
-add_filter( 'gform_entry_meta', 'add_meta_to_entry', 10, 2);
+// add_filter( 'gform_entry_meta', 'add_meta_to_entry', 10, 2);
 
 add_filter('gform_pre_render_15', 'add_readonly_script');
 
@@ -102,7 +102,7 @@ function display_choice_result( $form ) {
 	        $content_output .= '<div class="financing-box">' . get_finance_options( $system_price, $warranty_price ) . '</div>';
 
 	        $content_output .= '<div class="highlight-box">';
-	        $content_output .= '<p>Choose a <b>Payne Certified Dealer</b> & accept quote below to submit your contact information and schedule a <b>No Cost Installation Inspection</b> to verify the equipment selected is correct and provide an exact installation charge.</p>';
+	        $content_output .= '<p>Upon accepting and submitting your quote request, a <b>Payne Certified Dealer</b> will contact you within 24 hours to schedule a <b>No Cost Installation Inspection</b> to verify the equipment selected is correct and provide an exact installation charge.</p>';
 	        $content_output .= '</div>';
 
 	        //loop back through form fields to get html field (id 14 on my form) that we are populating with the data gathered above
