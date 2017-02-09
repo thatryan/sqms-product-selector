bee816f0b709fa93fd6d098a30430e01923d6303
Ryan Olson <ryan@thatryan.com>
2017-02-09 10:24:59 -0800

    more docs

--- includes/capabilities.php
+++ includes/capabilities.php
@@ -1,28 +1,44 @@
 <?php
-add_filter( 'user_has_cap', 'jt_user_has_cap_filter', 10, 4 );
+/**
+ * Set capabilities so that logged in dealers, who are viewing their own page, can reply
+ * to reviews, which are Gravity View notes.
+ */
+add_filter( 'user_has_cap', 'sqms_user_has_cap_filter', 10, 4 );
 
-function jt_user_has_cap_filter( $allcaps, $caps, $args, $user ) {
+/**
+ * Add capabilites to logged in dealers so they can see/reply to "reviews" with GF entry notes.
+ * @param  array $allcaps Current dealer's capabilities
+ * @param  array $caps    Required cap to do this stuff
+ * @param  array $args    Other arguments, not used here
+ * @param  object $user    WP_User object
+ * @return array          New capabilities set
+ */
+function sqms_user_has_cap_filter( $allcaps, $caps, $args, $user ) {
 
-    if ( ! is_singular( 'sqms_payne_dealer' ) )
-        return $allcaps;
+	// Only do this mess if the page being viewed is a dealer landing page
+	if ( ! is_singular( 'sqms_payne_dealer' ) ) {
+		return $allcaps;
+	}
 
-    // This is meta from a dropdown of CPT that is added to user profield via CMB2
-    $dealer_slug = get_user_meta( get_current_user_id(), 'sqms-product-dealer-id', true );
+	// This is meta from a dropdown of CPT that is added to user profield via CMB2
+	$dealer_slug = get_user_meta( get_current_user_id(), 'sqms-product-dealer-id', true );
 
-    if ( $dealer_slug ) {
+	if ( $dealer_slug ) {
 
-        // This is the only (ugly ass) way I found to get the CPT ID via the page slug
-        $page_path = 'sqms_payne_dealer/' . $dealer_slug;
-        $dealer_page = get_page_by_path( basename( untrailingslashit( $page_path ) ), OBJECT, 'sqms_payne_dealer');
+		// This is the only (ugly ass) way I found to get the CPT ID via the page slug
+		$page_path = 'sqms_payne_dealer/' . $dealer_slug;
+		$dealer_page = get_page_by_path( basename( untrailingslashit( $page_path ) ), OBJECT, 'sqms_payne_dealer');
 
-        if ( $dealer_page->ID === get_queried_object_id() ) {
+		// Make sure the page being viewed belongs to the logged in dealer
+		if ( $dealer_page->ID === get_queried_object_id() ) {
 
-            $needed_caps = array( 'gravityforms_view_entry_notes', 'gravityview_add_entry_notes', 'gravityview_view_entry_notes', 'gravityforms_edit_entry_notes' );
+			$needed_caps = array( 'gravityforms_view_entry_notes', 'gravityview_add_entry_notes', 'gravityview_view_entry_notes', 'gravityforms_edit_entry_notes' );
 
-            foreach ( $needed_caps as $cap )
-                $allcaps[ $cap ] = true;
-        }
-    }
+			foreach ( $needed_caps as $cap ) {
+				$allcaps[ $cap ] = true;
+			}
+		}
+	}
 
-    return $allcaps;
+	return $allcaps;
 }
--- sqms-product-selector.php
+++ sqms-product-selector.php
@@ -4,45 +4,43 @@
 	Plugin URI: https://github.com/thatryan
 	Description: Register product and handle logic for chooser
 	Author: Ryan Olson
-	Version: 1.1.4
+	Version: 1.1.5
 	Author URI: http://thatryan.com
  */
 
 add_action( 'plugins_loaded', array ( Product_Selector::get_instance(), 'plugin_setup' ) );
 
 class Product_Selector {
+
 	/**
 	 * Plugin instance.
-	 *
 	 * @see get_instance()
 	 * @type object
 	 */
 	protected static $instance = NULL;
 
-
 	/**
 	 * URL to this plugin's directory.
-	 *
 	 * @type string
 	 */
 	public $plugin_url = '';
 
-	public $plugin_version = '1.1.4';
+	/**
+	 * Current version of the plugin.
+	 * @type string
+	 */
 
+	public $plugin_version = '1.1.5';
 
 	/**
 	 * Path to this plugin's directory.
-	 *
 	 * @type string
 	 */
 	public $plugin_path = '';
 
-
 	/**
 	 * Access this plugin’s working instance
-	 *
 	 * @wp-hook plugins_loaded
-	 * @since   1.0.0
 	 * @return  object of this class
 	 */
 	public static function get_instance() {
@@ -53,18 +51,15 @@ class Product_Selector {
 
 	}
 
-
 	/**
 	 * Used for regular plugin work.
-	 *
 	 * @wp-hook plugins_loaded
-	 * @since   1.0.0
 	 * @return  void
 	 */
 	public function plugin_setup() {
 
-		$this->plugin_url    = plugins_url( '/', __FILE__ );
-		$this->plugin_path   = plugin_dir_path( __FILE__ );
+		$this->plugin_url 		= plugins_url( '/', __FILE__ );
+		$this->plugin_path 	= plugin_dir_path( __FILE__ );
 
 		define( 'SQMS_PLUGIN_NAME', 'sqms-product-selector' );
 		define( 'SQMS_PLUGIN_NAME_PLUGIN', 'sqms-product-selector/sqms-product-selector.php' );
@@ -85,92 +80,103 @@ class Product_Selector {
 
 	}
 
+	/**
+	 * If a dealer is logging in, redirect them to the page
+	 * showing their leads to report on.
+	 * @param  string $redirect_to Redirect destination URL
+	 * @param  string $request     Requested redirect destination URL
+	 * @param  object $user        WP_User object
+	 * @return string              The URL to redirec to
+	 */
 	public function dealer_login_redirect( $redirect_to, $request, $user  ) {
 
+		// A temp user to bypass the coming soon page for demos
 		if( $user->user_login == 'preview' ) {
 			return home_url();
 		}
+
 		//is there a user to check?
 		if ( isset( $user->roles ) && is_array( $user->roles ) ) {
 
 			//check for dealers
 			if ( in_array( 'subscriber', $user->roles ) ) {
 				// redirect them to the dealer leads list
-				return get_permalink( 138 ); //LIVE SITE
-				// return get_permalink( 899 ); // LOCAL SITE
-			} else {
+				return get_permalink( 138 );
+				}
+				else {
+					// Not a dealer, abort
+					return $redirect_to;
+				}
+			}
+			else {
 				return $redirect_to;
 			}
-		} else {
-			return $redirect_to;
-		}
 	}
 
 	/**
 	 * Constructor.
-	 *
 	 * @see plugin_setup()
-	 * @since 1.0.0
 	 */
 	public function __construct() {
-
 		add_action( 'wp_enqueue_scripts', array( $this, 'header_assets' ) );
 	}
 
-
+	/**
+	 * Register and enqueue scripts and styles for this plugin
+	 * @return void Scripts & styles added to header
+	 */
 	public function header_assets() {
+
+		// Register styles
 		wp_register_style( 'vex', $this->plugin_url . 'assets/css/vex.css', array(), $this->plugin_version );
 		wp_register_style( 'vex-theme', $this->plugin_url . 'assets/css/vex-theme-default.css', array(), $this->plugin_version );
 
+		// Register scripts
 		wp_register_script( 'vex-script', $this->plugin_url . 'assets/js/vex.combined.min.js', array ('jquery'), $this->plugin_version, false );
 		wp_register_script( 'load-report-form-script', $this->plugin_url . 'assets/js/load-report-form.js', array ('jquery'), $this->plugin_version, false );
 		wp_register_script( 'load-zip-form', $this->plugin_url . 'assets/js/load-zip-form.js', array ('jquery'), $this->plugin_version, false );
 		wp_register_script( 'jquery-validate', $this->plugin_url . 'assets/js/jquery.validate.min.js', array ('jquery'), $this->plugin_version, false );
 		wp_register_script( 'sqms-prod-select-script', $this->plugin_url . 'assets/js/sqms-product-selector.js', array ('jquery'), $this->plugin_version, false );
 
+		// Enqueue styles
+		wp_enqueue_style( 'dashicons' );
 		wp_enqueue_style( 'sqms-prod-select', $this->plugin_url . 'assets/css/sqms-product-selector.css', array(), $this->plugin_version );
 		wp_enqueue_style( 'vex' );
 		wp_enqueue_style( 'vex-theme' );
+
+		// Enqueue scripts
 		wp_enqueue_script('sqms-prod-select-script');
 		wp_enqueue_script('vex-script');
-		wp_enqueue_style( 'dashicons' );
+
 	}
 
 	/**
 	 * Run install related functionality
-	 *
-	 * @since  1.0.0
-	 *
 	 * @return void
 	 */
 	public function install() {
 
+		// Get our custom post types and taxonomies
 		require_once( 'includes/post-type.php' );
-		sqms_register_productselector_post_type();
 
+		// Permalinks! Refresh them
 		flush_rewrite_rules();
 
 	}
 
-
 	/**
 	 * Handles uninstalling plugin
-	 *
-	 * @since  1.0.0
-	 *
 	 * @return void
 	 */
 	public function uninstall() {
 
 		$productselectors = get_posts( array(
-			'post_type'      => 'sqms_prod_select',
+			'post_type' => array( 'sqms_prod_select', 'sqms_payne_dealer' ),
 			'posts_per_page' => -1,
 		) );
 
-		foreach( $productselectors as $login ) {
-
-			 wp_delete_post( $login->ID, true);
-
+		foreach( $productselectors as $post ) {
+			 wp_delete_post( $post->ID, true);
 		}
 
 		flush_rewrite_rules();
