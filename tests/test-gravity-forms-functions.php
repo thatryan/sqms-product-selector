b5bcc436bed66c3e92c0b78f85c40f7e874ec76e
Ryan Olson <ryan@thatryan.com>
2017-03-15 12:23:07 -0700

    start working on unit testing

---
+++ tests/test-gravity-forms-functions.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Sqms_Product_Selector
+ */
+
+/**
+ * Sample test case.
+ */
+class SPS_GF_Tests extends WP_UnitTestCase {
+
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+
+}
