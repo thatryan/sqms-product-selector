2d6993ec0aa70a2fc078266ca3355ac6d80b9edc
Ryan Olson <ryan@thatryan.com>
2017-07-24 09:25:19 -0700

    Create a cookie on form submission to check with unbounce popup

--- includes/gravity-forms-functions.php
+++ includes/gravity-forms-functions.php
@@ -417,7 +417,7 @@ function custom_confirmation( $confirmation, $form, $entry, $ajax ) {
 	$conversion_code = '<!-- Google Code for Instant Quote Form Conversion Page --><script type="text/javascript">/* <![CDATA[ */var google_conversion_id = 856718203;var google_conversion_language = "en";var google_conversion_format = "3";var google_conversion_color = "ffffff";var google_conversion_label = "62pkCKSq8m8Q-_bBmAM";var google_remarketing_only = false;/* ]]> */</script><script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js"></script><noscript><div style="display:inline;"><img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/856718203/?label=62pkCKSq8m8Q-_bBmAM&amp;guid=ON&amp;script=0"/></div></noscript>';
 
 	if( $form['id'] == 12 ) {
-	$confirmation .= '<h4>You will be contacted by an HVAC Instant Quote teammember within 24 hours to schedule your in home visit.</h4>';
+	$confirmation .= '<h4>You will be contacted by an HVAC Instant Quote team member within 24 hours to schedule your in home visit.</h4>';
 	$confirmation .= '<p>A copy of your quote information has been emailed to you. You may also download a PDF copy below.</p>';
 	$confirmation .= do_shortcode( '[gravitypdf name="Client Copy" id="57a03bc2e0cc7" class="button dealer-pdf" entry='.$entry['id'].' text="Download PDF"]' );
 	}
@@ -578,6 +578,8 @@ function choose_new_dealer( $form ) {
  * @return void        adds script for GTM
  */
 function add_gtm_submission( $entry, $form ) {
+	setcookie( 'quotesubmitted', 1, strtotime( '+30 days' ), COOKIEPATH, COOKIE_DOMAIN, false, false );
+
 	$event 				= 'quoteFormSubmit';
 	$event_category 	= 'Forms';
 	$event_action 		= 'Submission';
